d160e62002c1ff62c57c823cc09fc765fbb61f7d
TylersDesk <tymayn@gmail.com>
2015-08-28 00:21:40 -0600

    Added Fab Speed Dial Menu for Testing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,5 @@ angular.module( 'dgm3760', [ 'ngMaterial','ngMdIcons','dgm3760.controllers' ] )
 .config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('teal')
-    .accentPalette('amber')
-    .dark();
+    .accentPalette('amber');
 });
